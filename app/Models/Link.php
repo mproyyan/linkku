d68607c9ed1cbd1fd4b7cc990a6c1e16671de798
royyan <mproyyan@gmail.com>
2022-04-04 18:55:25 +0700

    create link model and table

---
+++ app/Models/Link.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Link extends Model
+{
+    use HasFactory, HasSlug;
+
+    protected $fillable = ['has', 'title', 'slug', 'url', 'description', 'excerpt', 'clicks', 'views',];
+
+    const VISIBILITY = [1, 2];
+    const PUBLIC = 1;
+    const PRIVATE = 2;
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+}
