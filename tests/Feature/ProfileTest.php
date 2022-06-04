72a57b7f7edad75c1dbc43c6db801b167dcb5198
royyan <mproyyan@gmail.com>
2022-06-04 09:03:33 +0700

    test[feature]: create setup function in ProfileTest to reduce repetitive code

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -10,84 +10,97 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
+use Tests\Trait\ResponseStructure;
+use Tests\Trait\WithUser;
 
 class ProfileTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithUser, ResponseStructure;
 
-    public function test_get_the_owner_of_the_account()
+    private $user2;
+    private $token;
+    private $ownerToken;
+
+    public function setUp(): void
     {
-        $user = User::factory()->create();
-        $token = $user->createToken('main')->plainTextToken;
+        parent::setUp();
 
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->getJson('/api/user/' . $user->username);
+        $this->setupUser();
+        $this->user2 = User::factory()->create();
+        $this->token = $this->user2->createToken('main')->plainTextToken;
+        $this->ownerToken = $this->user->createToken('main')->plainTextToken;
+    }
+
+    public function test_get_user_profile_as_owner()
+    {
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->ownerToken)
+            ->getJson('/api/user/' . $this->user->username);
 
         $response->assertStatus(200)
             ->assertJson(
                 fn (AssertableJson $json) =>
-                $json->where('user.name', $user->name)
-                    ->where('user.email', $user->email)
+                $json->where('user.name', $this->user->name)
+                    ->where('user.email', $this->user->email)
                     ->where('owner', true)
                     ->missing('user.password')
                     ->etc()
             );
     }
 
-    public function test_get_account_but_not_the_owner()
+    public function test_get_user_profile_but_not_as_owner()
     {
-        $user = User::factory()->create();
-        $user2 = User::factory()->create();
-        $token = $user2->createToken('main')->plainTextToken;
-
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->getJson('/api/user/' . $user->username);
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+            ->getJson('/api/user/' . $this->user->username);
 
         $response->assertStatus(200)
             ->assertJson(
                 fn (AssertableJson $json) =>
-                $json->where('user.name', $user->name)
-                    ->where('user.email', $user->email)
+                $json->where('user.name', $this->user->name)
+                    ->where('user.email', $this->user->email)
                     ->where('owner', false)
                     ->missing('user.password')
                     ->etc()
             );
     }
 
-    public function test_failed_get_user_profile()
+    public function test_user_profile_not_found()
     {
         $response = $this->getJson('/api/user/salah');
-        $response->assertStatus(404);
+
+        $response->assertStatus(404)
+            ->assertJsonStructure($this->standardApiProblemStructure);
     }
 
     public function test_get_authenticated_user()
     {
-        $user = User::factory()->create();
-        $token = $user->createToken('main')->plainTextToken;
-
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->getJson('/api/user/');
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->ownerToken)
+            ->getJson('/api/user');
 
         $response->assertStatus(200)
             ->assertJson(
                 fn (AssertableJson $json) =>
-                $json->where('user.name', $user->name)
-                    ->where('user.email', $user->email)
+                $json->where('user.name', $this->user->name)
+                    ->where('user.email', $this->user->email)
                     ->missing('user.password')
                     ->etc()
             );
     }
 
+    public function test_failed_get_authenticated_user_when_token_not_included()
+    {
+        $response = $this->getJson('/api/user');
+
+        $response->assertStatus(401)
+            ->assertJsonStructure($this->standardApiProblemStructure);
+    }
+
     public function test_update_banner()
     {
-        $user = User::factory()->create();
-        $token = $user->createToken('main')->plainTextToken;
-
         Storage::fake('public');
         $file = UploadedFile::fake()->image('banner.png', 800, 500);
 
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->putJson("/api/user/$user->username/update-banner", [
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->ownerToken)
+            ->putJson("/api/user/{$this->user->username}/update-banner", [
                 'banner' => $file
             ]);
 
@@ -99,20 +112,17 @@ class ProfileTest extends TestCase
 
     public function test_user_banner_replaced_when_banner_got_new_update()
     {
-        $user = User::factory()->create();
-        $token = $user->createToken('main')->plainTextToken;
-
         Storage::fake('public');
         $file1 = UploadedFile::fake()->image('banner-1.jpg', 800, 500);
         $file2 = UploadedFile::fake()->image('banner-2.jpg', 800, 500);
 
-        $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->putJson("/api/user/$user->username/update-banner", [
+        $this->withHeader('Authorization', 'Bearer ' . $this->ownerToken)
+            ->putJson("/api/user/{$this->user->username}/update-banner", [
                 'banner' => $file1
             ]);
 
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->putJson("/api/user/$user->username/update-banner", [
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->ownerToken)
+            ->putJson("/api/user/{$this->user->username}/update-banner", [
                 'banner' => $file2
             ]);
 
@@ -125,33 +135,28 @@ class ProfileTest extends TestCase
 
     public function test_update_banner_failed_when_user_update_an_account_that_doesnt_belong_to_him()
     {
-        $user = User::factory()->create();
-        $user2 = User::factory()->create();
-        $token = $user2->createToken('main')->plainTextToken;
-
         Storage::fake('public');
         $file = UploadedFile::fake()->image('banner.png', 800, 500);
 
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->putJson("/api/user/$user->username/update-banner", [
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+            ->putJson("/api/user/{$this->user->username}/update-banner", [
                 'banner' => $file
             ]);
 
-        $response->assertStatus(403);
+        $response->assertStatus(403)
+            ->assertJsonStructure($this->standardApiProblemStructure)
+            ->assertJsonPath('detail', 'You cannot update profile that are not yours');
     }
 
     public function test_update_user_profile()
     {
-        $user = User::factory()->create();
-        $token = $user->createToken('main')->plainTextToken;
-
         Storage::fake('public');
         $file = UploadedFile::fake()->image('profile-picture.jpeg', 500, 400);
 
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->putJson("/api/user/$user->username/update-profile", [
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->ownerToken)
+            ->putJson("/api/user/{$this->user->username}/update-profile", [
                 'name' => 'Muhammad Pandu Royyan',
-                'username' => $user->username,
+                'username' => $this->user->username,
                 'image' => $file
             ]);
 
@@ -160,7 +165,7 @@ class ProfileTest extends TestCase
             ->assertJson(
                 fn (AssertableJson $json) =>
                 $json->where('user.name', 'Muhammad Pandu Royyan')
-                    ->where('user.username', $user->username)
+                    ->where('user.username', $this->user->username)
                     ->missing('user.password')
                     ->etc()
             );
@@ -170,20 +175,17 @@ class ProfileTest extends TestCase
 
     public function test_old_image_deleted_when_got_new_image()
     {
-        $user = User::factory()->create();
-        $token = $user->createToken('main')->plainTextToken;
-
         Storage::fake('public');
         $file1 = UploadedFile::fake()->image('profile-picture-1.jpeg', 500, 400);
         $file2 = UploadedFile::fake()->image('profile-picture-2.jpeg', 500, 400);
 
-        $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->putJson("/api/user/$user->username/update-profile", [
+        $this->withHeader('Authorization', 'Bearer ' . $this->ownerToken)
+            ->putJson("/api/user/{$this->user->username}/update-profile", [
                 'image' => $file1
             ]);
 
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->putJson("/api/user/$user->username/update-profile", [
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->ownerToken)
+            ->putJson("/api/user/{$this->user->username}/update-profile", [
                 'image' => $file2
             ]);
 
@@ -196,40 +198,29 @@ class ProfileTest extends TestCase
 
     public function test_update_profile_failed_when_given_username_already_used()
     {
-        $existUser = User::factory()->create();
-        $user = User::factory()->create();
-        $token = $user->createToken('main')->plainTextToken;
-
         Storage::fake('public');
         $file = UploadedFile::fake()->image('profile-picture.jpeg', 500, 400);
 
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->putJson("/api/user/$user->username/update-profile", [
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->ownerToken)
+            ->putJson("/api/user/{$this->user->username}/update-profile", [
                 'name' => 'Muhammad Pandu Royyan',
-                'username' => $existUser->username,
+                'username' => $this->user2->username,
                 'image' => $file
             ]);
 
         $response->assertStatus(422)
-            ->assertJsonPath('errors.username.0', 'The username has already been taken.');
+            ->assertJsonStructure($this->standardApiProblemStructure);
     }
 
     public function test_update_profile_failed_when_user_update_an_account_that_doesnt_belong_to_him()
     {
-        $user = User::factory()->create();
-        $user2 = User::factory()->create();
-        $token = $user2->createToken('main')->plainTextToken;
-
-        Storage::fake('public');
-        $file = UploadedFile::fake()->image('profile-picture.jpeg', 500, 400);
-
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->putJson("/api/user/$user->username/update-profile", [
+        $response = $this->withHeader('Authorization', 'Bearer ' . $this->token)
+            ->putJson("/api/user/{$this->user->username}/update-profile", [
                 'name' => 'Muhammad Pandu Royyan',
-                'username' => $user->username,
-                'image' => $file
             ]);
 
-        $response->assertStatus(403);
+        $response->assertStatus(403)
+            ->assertJsonStructure($this->standardApiProblemStructure)
+            ->assertJsonPath('detail', 'You cannot update profile that are not yours');
     }
 }
